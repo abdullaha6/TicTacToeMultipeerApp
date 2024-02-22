57b0d787be13952d2a22a9245bd5a843c78468aa
Abdullah Ajmal <abdullaha6@gmail.com>
2024-02-22 10:49:26 +0530

    Initial Commit

---
+++ TicTacToeMultipeerApp/TicTacToeMultipeerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeMultipeerAppApp.swift
+//  TicTacToeMultipeerApp
+//
+//  Created by Abdullah Ajmal on 2024-02-22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeMultipeerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
